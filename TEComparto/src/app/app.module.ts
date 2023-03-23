9b89d76a3a583aab4598377666ee9d9d59e1ad2a
alexdennis2001 <alejandrodennis2001@outlook.com>
2023-03-22 20:46:00 -0600

    GetMovie done

--- TEComparto/src/app/app.module.ts
+++ TEComparto/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DashboardpComponent } from './dashboardp/dashboardp.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
